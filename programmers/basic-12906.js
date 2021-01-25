8f0416c311ee9abe42e6d0bcdf09c8454455c9f0
yoooozi <j16412000kr@gmail.com>
2021-01-25 22:23:42 +0900

    programmers 12906, 42840

---
+++ programmers/basic-12906.js
@@ -0,0 +1,36 @@
+/**
+배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다. 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다. 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
+
+arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+배열 arr의 크기 : 1,000,000 이하의 자연수
+배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
+
+입출력 예
+arr	answer
+[1,1,3,3,0,1,1]	[1,3,0,1]
+[4,4,4,3,3]	[4,3]
+
+입출력 예 설명
+입출력 예 #1,2
+문제의 예시와 같습니다.
+ */
+
+function solution(arr) {
+  return arr.reduce((acc, current) => {
+    if (!acc.length || acc[acc.length - 1] !== current) {
+      acc.push(current);
+    }
+    return acc;
+  }, []);
+}
+
+/*
+COMMENT:
+- reduce로 체크해도 될것 같고, filter로 비교해도 충분 할 것으로 보임
+  - return arr.filter((value, index) => { return value !== arr[index+1]; })
+  - 하지만 이 코드는 index + 1에서 마지막에 도달할 경우 undefined 가 나올텐데, 조금 문제가 될 가능성을 두는게 아닐까 생각됨
+*/
